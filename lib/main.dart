df4579563624a52fc992c99e076a83e5d0f7844c
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2023-10-26 09:02:49 +0200

    Image

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+
+class Home extends StatelessWidget {
+  // const ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('My first app'),
         centerTitle: true,
         backgroundColor: Colors.red,
       ),
       body: Center(
-        child: Text(
-            'Hello guys',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower',
-            )
-        ),
+        child: Image.asset('assets/image2.jpeg'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -27,6 +28,7 @@ void main() {
         child: Text('click'),
         backgroundColor: Colors.red,
       ),
-    ),
-  ));
-}
+    );
+  }
+  }
+
